a9642abc9d01d8c23c40be0736ab5b8a70703793
Michael Solati <mkslt04@gmail.com>
2020-08-26 15:30:06 -0700

    refactor: remove `_redirects.yaml` by migrating redirects to `firebase.json`

--- test/integration/build-test.js
+++ test/integration/build-test.js
@@ -27,7 +27,6 @@ describe('Build test', function () {
       path.join('en', 'tags', 'progressive-web-apps', 'feed.xml'),
       path.join('images', 'favicon.ico'),
       path.join('images', 'lockup.svg'),
-      '_redirects.yaml',
       'app.css',
       'bootstrap.js',
       'manifest.webmanifest',
